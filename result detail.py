a731afe0329de697657c5f509fbb23b931dea0b3
Karmjitssingh <87595074+Karmjitssingh@users.noreply.github.com>
2021-07-18 03:44:46 -0700

    result detail in python
    use python

---
+++ result detail.py
@@ -0,0 +1,12 @@
+a=int(input("Enter the english subject number"))
+b=int(input("Enter the hindi subject number"))
+c=a+b
+print(c)
+d=int(input("Entre the total mark"))
+e=c*100/d
+print(e)
+if(e>=50):
+  print("Result is pass")
+else:
+  print("fail")
+
